98c28c59139c7e02abd4c18839337096815e5619
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-10-07 12:56:07 -0700

    completed guided project

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmpList.java
@@ -2,12 +2,10 @@ package com.lambdaschool.webemployees;
 
 import java.util.ArrayList;
 
-public class EmpList
-{
+public class EmpList {
     public ArrayList<Employee> empList = new ArrayList<Employee>();
 
-    public EmpList()
-    {
+    public EmpList() {
         empList.add(new Employee("Steve", "Green", 45000, true, 1, 1));
         empList.add(new Employee("Sam", "Ford", 80000, true, 1, 1));
         empList.add(new Employee("John", "Jones ", 75000, true, 1, 1));
@@ -15,9 +13,9 @@ public class EmpList
 
     public Employee findEmployee(CheckEmployee tester)
     {
-        for (Employee e : empList)
+        for (Employy e : empList)
         {
-            if (tester.test(e))
+            if (tester.testie(e))
             {
                 return e;
             }
@@ -27,16 +25,15 @@ public class EmpList
 
     public ArrayList<Employee> findEmployees(CheckEmployee tester)
     {
-        ArrayList<Employee> tempEmpList = new ArrayList<>();
+        ArrayList<Employee> TempEmpList = new ArrayList<>();
 
-        for (Employee e: empList)
+        for (Employee : empList)
         {
             if (tester.test(e))
             {
                 tempEmpList.add(e);
             }
         }
-
         return tempEmpList;
     }
 }
